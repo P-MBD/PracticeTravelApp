b3ff5d436287bb8675776739ef70b7082b8c7a19
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-01-21 11:01:43 +0330

    Creating First Component

---
+++ src/screens/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {SafeAreaView,View} from "react-native";
+import Title from "../../components/Title/index";
+const Home = () => {
+    return (
+        <SafeAreaView>
+             <View>
+           <Title />
+         </View>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
