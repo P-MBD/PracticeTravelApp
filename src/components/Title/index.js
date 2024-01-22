31feedce621c6765723723903c1cc0cdb5a45200
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-01-22 12:04:42 +0330

    props

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,11 +1,25 @@
 import React from "react";
-import {Text } from "react-native";
+import {Text, View, Image } from "react-native";
 import styles from "./styles";
-const Title = () => {
-    return(
-        <Text style={styles.title}> Title component </Text>
-    );
-};
 
+const Title = (props) => {
+    return (
+        <View  style={styles.content}>
+          <Image source={props.uri} style={styles.image}/>
+          <Text>{props.title}</Text>
+          <Text>{props.description}</Text>
+          <Text>{props.price}</Text>
+        </View>
+      );
+}
 export default Title;
+// const Title = ({text}) => {
+//     console.log('text:>>', text);
+//     return(
+//         <Text style={styles.title}> {text} </Text>
+//         );};
+// Title.defaultProps = {
+//     text: 'Default Text'
+// };
+
 
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -5,10 +5,26 @@ const Home = () => {
     return (
         <SafeAreaView>
              <View>
-           <Title />
+           {/* <Title text='My First Component' /> */}
+           <Title 
+               title="شیراز"
+               description="باغ ارم، آرامگاه حافظ"
+               price="1000000"
+               uri={require('../../../assets/images/shiraz.jpg')}
+           />
+             <Title 
+               title="قزوین"
+               description="چهل ستون، سعد السلطنه"
+               price="1000000"
+               uri={require('../../../assets/images/qazvin.jpg')}
+           />
+             <Title 
+               title="اصفهان"
+               description="پل خواجوی اصفهان"
+               price="1000000"
+               uri={require('../../../assets/images/esfehan.webp')}
+           />
          </View>
         </SafeAreaView>
-    )
-}
-
+    );};
 export default Home;
